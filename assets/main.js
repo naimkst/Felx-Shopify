8cca40cf6a943d3ec1affcc9a5b0d387d08564cb
naimkst <najmulspark@gmail.com>
2022-02-10 19:08:20 +0600

    pages done

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,31 @@
-(function($) {
+(function ($) {
   "use strict";
   /*-------------------------------------------
   preloader active
   --------------------------------------------- */
-  jQuery(window).on('load',function() {
+  jQuery(window).on('load', function () {
     jQuery('.preloader').fadeOut('slow');
   });
 
   /*-------------------------------------------
   Sticky Header
   --------------------------------------------- */
-  $(window).on('scroll', function(){
-    if( $(window).scrollTop()>80 ){
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 80) {
       $('#sticky').addClass('stick');
     } else {
       $('#sticky').removeClass('stick');
     }
-  }); 
-  
-  jQuery(document).ready(function(){
+  });
+
+  jQuery(document).ready(function () {
     /*-------------------------------------------
     filter-box active
     --------------------------------------------- */
-    jQuery('.finter-button').on('click',function() {
+    jQuery('.finter-button').on('click', function () {
       jQuery('.filter-box').addClass('show');
     });
-    jQuery('.close-filter').on('click',function() {
+    jQuery('.close-filter').on('click', function () {
       jQuery('.filter-box').removeClass('show');
     });
     /*-------------------------------------------
@@ -36,7 +36,7 @@
       easingType: 'linear',
       scrollSpeed: 300,
       animation: 'fade'
-    }); 
+    });
     /*-------------------------------------------
     post-thumb-slide active
     --------------------------------------------- */
@@ -65,37 +65,54 @@
     /*-------------------------------------------
     js price range
     --------------------------------------------- */
-    $( function() {
-      $( "#slider-range" ).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 75, 300 ],
-        slide: function( event, ui ) {
-          $( "#amount1" ).val( "€" + ui.values[ 0 ]  );
-          $( "#amount2" ).val( "€" + ui.values[ 1 ] );
-        }
+
+    $(document).ready(function () {
+      var maxPrice = $('#max_price_amountss').attr("data-max");
+      var curencySymbol = $('#curency_symble').attr("data-symbol");
+
+      var curentMin = $('#amount1').attr("curent-min");
+      var curentMax = $('#amount2').attr("curent-max");
+      $(function () {
+
+        console.log(parseInt(maxPrice));
+        $("#slider-range").slider({
+          range: true,
+          min: 0,
+          max: parseInt(maxPrice),
+          values: [
+            curentMin ? parseInt(curentMin) : 0,
+            curentMax ? parseInt(curentMax) : maxPrice
+          ],
+          slide: function (event, ui) {
+
+            $("#amount1").val(curencySymbol + ui.values[0]);
+            $("#amount2").val(curencySymbol + ui.values[1]);
+          }
+
+        });
+        $("#amount1").val(curencySymbol + $("#slider-range").slider("values", 0));
+        $("#amount2").val(curencySymbol + $("#slider-range").slider("values", 1));
       });
-      $( "#amount1" ).val( "€" + $( "#slider-range" ).slider( "values", 0 ) );
-      $( "#amount2" ).val( "€" + $( "#slider-range" ).slider( "values", 1 ) );
-    } );
+    });
+
+
     /*-------------------------------------------
     js price range mobile
     --------------------------------------------- */
-    $( function() {
-      $( "#slider-range-m" ).slider({
+    $(function () {
+      $("#slider-range-m").slider({
         range: true,
         min: 0,
         max: 500,
-        values: [ 75, 300 ],
-        slide: function( event, ui ) {
-          $( "#amount1-m" ).val( "€" + ui.values[ 0 ]  );
-          $( "#amount2-m" ).val( "€" + ui.values[ 1 ] );
+        values: [75, 300],
+        slide: function (event, ui) {
+          $("#amount1-m").val("€" + ui.values[0]);
+          $("#amount2-m").val("€" + ui.values[1]);
         }
       });
-      $( "#amount1-m" ).val( "€" + $( "#slider-range-m" ).slider( "values", 0 ) );
-      $( "#amount2-m" ).val( "€" + $( "#slider-range-m" ).slider( "values", 1 ) );
-    } );
+      $("#amount1-m").val("€" + $("#slider-range-m").slider("values", 0));
+      $("#amount2-m").val("€" + $("#slider-range-m").slider("values", 1));
+    });
     /*-------------------------------------------
     js counterUp
     --------------------------------------------- */
@@ -103,7 +120,97 @@
       delay: 10,
       time: 1000
     });
+    /*-------------------------------------------
+    Product Quantity JS
+    --------------------------------------------- */
+    var productQty = $(".product");
+    productQty.append('<button class="inc qty-btn">+</button>');
+    productQty.append('<button class= "dec qty-btn">-</button>');
+    $('.qty-btn').on('click', function (e) {
+      e.preventDefault();
+      var $button = $(this);
+      var oldValue = $button.parent().find('input').val();
+      if ($button.hasClass('inc')) {
+        var newVal = parseFloat(oldValue) + 1;
+      } else {
+        if (oldValue > 1) {
+          var newVal = parseFloat(oldValue) - 1;
+        } else {
+          newVal = 1;
+        }
+      }
+      $button.parent().find('input').val(newVal);
+      console.log(newVal);
+      $('input#quantity').val(newVal);
+    });
+
+    //Add To Cart
+    $('.skipChart').on('click', function (e) {
+      var form = $('#addToCart');
+      e.preventDefault()
+      $.ajax({
+        type: 'POST',
+        url: '/cart/add',
+        dataType: 'json',
+        data: form.serialize(),
+        success: function (data) {
+          var current_url = window.location.href;
+          console.log(current_url);
+          window.location.href = current_url;
+
+        }
+      });
+    });
+
+    //Update To Cart Data
+    $('.updateSkipBtn').on('click', function (e) {
+      var form = $('#cartUpdate');
+      e.preventDefault()
+      $.ajax({
+        type: 'POST',
+        url: '/cart',
+        dataType: 'json',
+        data: form.serialize(),
+        success: function (data) {
+          var current_url = window.location.href;
+          console.log(data, current_url);
+          document.location.href = current_url;
+          $('div#offcanvasCart').toggleClass('show');
+          // $('div#offcanvasCart').css({
+          //   'visibility': 'visible'
+          // });
+        }
+      });
+    });
+
+
+    //Remove Product
+    $('.close-item').on('click', function (e) {
+      var form = $('#cartUpdate');
+      var User_id = $(this).attr('data-id');
+      console.log(User_id);
+      e.preventDefault()
+      $.ajax({
+        type: 'POST',
+        url: "/cart/change",
+        dataType: 'json',
+        data: {
+          "line": User_id,
+          "quantity": 0
+        },
+        success: function (data) {
+          var current_url = window.location.href;
+          document.location.href = current_url;
+
+        }
+      });
+    });
+
 
   });
+
+
+
+
 
 })(jQuery);
